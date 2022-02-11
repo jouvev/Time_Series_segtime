c6a68e0018698a4ccaec9c9145a46ffd1dac8f0d
Readix1 <romain.lenoir@etu.sorbonne-universite.fr>
2022-02-11 15:40:17 +0100

    ajout encoder

--- src/encoder.py
+++ src/encoder.py
@@ -81,3 +81,101 @@ class Res1DNet(nn.Module):
         x = self.layer3(x)
         x = self.layer4(x)
         return x, low_level_feat
+    
+
+class AMSP(nn.Module):
+    def __init__(self, input_channel, output_stride, BatchNorm):
+        super(AMSP, self).__init__()
+        
+        inplanes = min(input_channel*32, 1024)
+        amsp_channel = min(256, input_channel*8)
+        if output_stride == 16:
+            dilations = [1, 6, 12, 18]
+        elif output_stride == 8:
+            dilations = [1, 12, 24, 36]
+        else:
+            raise NotImplementedError
+            
+        self.amsp1 = _AMSPModule(inplanes, amsp_channel, 1, padding=0, dilation=dilations[0], BatchNorm=BatchNorm)
+        self.amsp2 = _AMSPModule(inplanes, amsp_channel, 3, padding=dilations[1], dilation=dilations[1], BatchNorm=BatchNorm)
+        self.amsp3 = _AMSPModule(inplanes, amsp_channel, 3, padding=dilations[2], dilation=dilations[2], BatchNorm=BatchNorm)
+        self.amsp4 = _AMSPModule(inplanes, amsp_channel, 3, padding=dilations[3], dilation=dilations[3], BatchNorm=BatchNorm)
+        self.global_avg_pool = nn.Sequential(nn.AdaptiveAvgPool1d(1),
+                                             nn.Conv1d(inplanes, amsp_channel, 1, stride=1, bias=False),
+                                             nn.ReLU())
+        self.conv1 = nn.Conv1d(amsp_channel*5, amsp_channel, 1, bias=False)
+        self.bn1 = BatchNorm(amsp_channel)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+        self._init_weight()
+
+    def forward(self, x):
+        x1 = self.amsp1(x)
+        x2 = self.amsp2(x)
+        x3 = self.amsp3(x)
+        x4 = self.amsp4(x)
+        x5 = self.global_avg_pool(x)
+        x5 = F.interpolate(x5, size=x4.size()[2:], mode='linear', align_corners=True)
+        x = torch.cat((x1, x2, x3, x4, x5), dim=1)
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        return self.dropout(x)
+
+    def _init_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv1d):
+                torch.nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, SynchronizedBatchNorm1d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm1d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+    
+class _AMSPModule(nn.Module):
+    def __init__(self, in_size, out_size, kernel_size, padding, dilation, BatchNorm):
+        super(_AMSPModule, self).__init__()
+        self.atrous_conv = nn.Conv1d(in_size, out_size, kernel_size=kernel_size,
+                                            stride=1, padding=padding, dilation=dilation, bias=False)
+        self.bn = BatchNorm(out_size)
+        self.relu = nn.ReLU()
+
+        self._init_weight()
+
+    def forward(self, x):
+        x = self.atrous_conv(x)
+        x = self.bn(x)
+
+        return self.relu(x)
+
+    def _init_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv1d):
+                torch.nn.init.kaiming_normal_(m.weight)
+            else:
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+                
+                
+                
+class Encoder(nn.Module):
+    def __init__(self, input_channel, output_stride=8, channel_last_data=True): #sync_bn=True,
+        super(Encoder, self).__init__()
+        BatchNorm = nn.BatchNorm1d
+        
+        self.resnet1d = Res1DNet(input_channel, Bottleneck1D, [2, 2, 4, 2], output_stride, nn.BatchNorm1d, pretrained=False)
+        
+        self.amsp = AMSP(input_channel, output_stride, BatchNorm)
+        self.channel_last_data = channel_last_data
+
+    def forward(self, input):
+        if self.channel_last_data:# swap data axis
+            input = input.permute(0,2,1)
+
+        x, low_level_feat = self.resnet1d(input)
+        x = self.amsp(x) 
+
+        return x, low_level_feat
